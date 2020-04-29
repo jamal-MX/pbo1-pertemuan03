ffabfc2669460e876cb29174a02f8dbe6b396d39
JAMAL-MX <pointleopard007@gmail.com>
2020-04-29 19:44:09 +0800

    Operator Binary Aritmatika

---
+++ src/operator/OperatorBinary.java
@@ -0,0 +1,22 @@
+package operator;
+
+public class OperatorBinary {
+    //operator (=) sebagai assigment/penugasan
+    //menempatkan nilai kedalam variable
+    //baris 5-7 adalah comment/komentar yang tidak dijalankan program
+
+    String nama =  "jamalludin";
+    int jumlahSks = 24;
+    double indeksPrestasiKumulatif = 3.89;
+
+    public static void main (String [] args){
+
+        System.out.println(9+3);
+        System.out.println(9-3);
+        System.out.println(9*3);
+        System.out.println(9%3);
+        System.out.println(9%2);
+
+
+    }
+}
